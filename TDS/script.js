60e89352f79e25a7e74a71c57481e26266d6f0c9
ewan <ewanwd2008@gmail.com>
2025-05-22 15:06:41 +0100

    8.9 bleh

--- TDS/script.js
+++ TDS/script.js
@@ -1,14 +1,1 @@
 TDS/script.js
-
-function toggleDropdown() {
-    const menu = document.getElementById("settingsMenu");
-    menu.classList.toggle("show");
-}
-window.addEventListener("click", function(event) {
-    const settingsButton = document.querySelector(".Settings");
-    const dropdown = document.getElementById("settingsMenu");
-    
-    if (!settingsButton.contains(event.target) && !dropdown.contains(event.target)) {
-        dropdown.classList.remove("show");
-    }
-});
